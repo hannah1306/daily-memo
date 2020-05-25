3daf2ecc49c199a2d3fd68791159fd425e5691da
hannah1306 <suhyun1306@gmail.com>
2020-05-25 17:01:08 +0900

    Add input box

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import { TextInput } from 'react-native';
 import styled from 'styled-components/native';
 
 export default function HomeScreen(){
 
+  const _createMemo = () => {
+    console.log(1);
+  }
+  
   return (
     <S.ChatView>
-      <S.ChatInput></S.ChatInput>
+      <S.MemoBox>
+        <S.MemoInput></S.MemoInput>
+        <S.MemoButton title="입력" onPress={_createMemo}></S.MemoButton>
+      </S.MemoBox>
     </S.ChatView>
   )
 }
@@ -15,6 +21,12 @@ const S: any = {};
 S.ChatView = styled.View`
   flex: 1;
 `;
-S.ChatInput = styled.TextInput`
+S.MemoBox = styled.View`
+  flex-direction: row;
+`;
+S.MemoInput = styled.TextInput`
   border: 1px solid red;
+`;
+S.MemoButton = styled.Button`
+
 `;
